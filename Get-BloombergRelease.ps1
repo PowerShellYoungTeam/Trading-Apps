46d0f4ae92349e79058048bf86aafd5503b9500c
Steven Wight (PoSHYoungTeam) <53131013+PowerShellYoungTeam@users.noreply.github.com>
2023-06-17 22:30:46 +0100

    Create Get-BloombergRelease.ps1

---
+++ Get-BloombergRelease.ps1
@@ -0,0 +1,55 @@
+function Get-BloombergRelease
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(ValueFromPipeline=$true,Position=0)]
+        $URL = https://www.bloomberg.com/professional/support/software-updates/
+    )
+ 
+    Begin {
+ 
+        $Start = Get-Date
+        Write-Verbose "Scraping $($URL) for data at $((Get-Date).ToString('yyyy-MM-dd HH:MM:ss'))"
+ 
+    }
+
+    Process {
+ 
+        Write-Verbose "Extracting Data from $($URL)"
+        $page = Invoke-WebRequest -Uri $URL
+ 
+        $TRwithData = $page.ParsedHtml.getElementsByTagName('tr') | Where-Object { $_.innerText  -like "Bloomberg Terminal - New/Upgrade Installation*"}
+        $HTMLwithData = $TRwithData.outerHTML
+ 
+        $firstString = '<td class="date">'
+        $secondString = '</td>'
+ 
+        #Regex pattern to compare two strings
+        $pattern = "$firstString(.*?)$secondString"
+ 
+        #Perform the operation
+        $ReleaseMonth = [regex]::Match($HTMLwithData,$pattern).Groups[1].Value
+ 
+        if ($null -eq $ReleaseMonth){
+ 
+            Write-Verbose "Can't find the Download in $($URL)"
+            break
+ 
+        } else {
+ 
+            Write-Verbose "Found $($ReleaseMonth) Month in $($URL)"
+ 
+        }
+    }
+ 
+    End {
+ 
+        $End = Get-Date
+ 
+        Write-Verbose "Script finished at $((Get-Date).ToString('yyyy-MM-dd HH:MM:ss'))"
+        Write-Verbose "This script took $((New-TimeSpan -Start $Start -End $End).TotalSeconds) seconds to complete"
+       
+        Return $ReleaseMonth
+    }
+}
